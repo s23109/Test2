552bc05e81204c75b20606b64f8bd5a6e3d05b7c
s23109 <s23109@pjwstk.edu.pl>
2022-06-14 08:40:40 +0200

    Models

---
+++ Test2/Models/Album.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test2.Models
+{
+    public class Album
+    {
+
+        public int IdAlbum { get; set; }
+
+        [MaxLength(30)]
+        public string AlbumName { get; set; }
+        
+        public DateTime PublishDate { get; set; }
+        
+        public int IdMusicLabel { get; set; }
+
+    }
+}
---
+++ Test2/Models/Musician.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test2.Models
+{
+    public class Musician
+    {
+        public int IdMusician { get; set; }
+        [MaxLength(30)]
+        public string FirstName { get; set; }
+        [MaxLength(50)]
+        public string LastName { get; set; }
+        [MaxLength(20)]
+        public string? NickName { get; set; }
+
+    }
+}
